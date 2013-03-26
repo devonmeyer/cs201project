f38f10215310cb794385cfaebe9caeefe31626ad
Tim Huang <timhuang@usc.edu>
2013-03-26 16:26:58 -0700

    Some interfaces for the conveyor family agents

---
+++ GlassLineFactory/src/interfaces/Conveyor.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public interface Conveyor {
+	
+	public abstract void msgPopUpLowered();
+	
+	public abstract void msgPopUpRaised();
+	
+	public abstract void msgSwitchOn();
+	
+	public abstract void msgSwitchOff();
+
+}
---
+++ GlassLineFactory/src/interfaces/PopUp.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface PopUp {
+	
+	public abstract void msgReadyToTakeGlass();
+	
+	public abstract void msgGlassProcessed();
+	
+	public abstract void msgHereIsGlass();
+
+}
