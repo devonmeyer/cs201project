455a330a2f5577cc4843d92d3f1676f8c773570e
mfmendiola <mmendiol@usc.edu>
2013-04-07 15:17:07 -0700

    updated Glass.java class added member variables (processes, ID)

--- GlassLineFactory/src/glassLine/Glass.java
+++ GlassLineFactory/src/glassLine/Glass.java
@@ -1,5 +1,35 @@
 package glassLine;
 
-public class Glass {
+import java.util.List;
 
+public class Glass {
+	/**
+	 * This class represents a piece of glass being processed by the factory.
+	 */
+	private int ID;    
+	private List<String> processes; // This contains the list of processes to be
+									// performed on glass
+	
+	public Glass(int id, List<String> processes) {
+		this.setID(id);
+		this.setProcesses(processes);
+	}
+	
+	/** Getters and Setters **/
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public List<String> getProcesses() {
+		return processes;
+	}
+
+	public void setProcesses(List<String> processes) {
+		this.processes = processes;
+	}
 }
