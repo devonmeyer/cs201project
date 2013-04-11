309a307f7a564c418818b6535fe061b845352e40
Tim Huang <timhuang@usc.edu>
2013-04-11 02:05:19 -0800

    agent initialization

--- GlassLineFactory/src/gui/panels/FactoryPanel.java
+++ GlassLineFactory/src/gui/panels/FactoryPanel.java
@@ -3,6 +3,7 @@ package gui.panels;
 
 import glassLine.*;
 import glassLine.agents.*;
+import glassLine.interfaces.*;
 import gui.drivers.FactoryFrame;
 
 import javax.swing.BoxLayout;
@@ -35,11 +36,34 @@ public class FactoryPanel extends JPanel
 	int agentNumber = 0;
 	
 	/*
-	 * Factory Agents, Conveyor Families
+	 * Factory Agents
 	 */
 	public GlassRobotAgent gRobot;
-	public OnlineWorkStationAgent()
+	public OnlineWorkStationAgent cutter;
+	public OnlineWorkStationAgent breakout;
+	public OnlineWorkStationAgent mbreakout;
+	public PopupAgent crossSeamer;
+	public PopupAgent grinder;
+	public PopupAgent drill;
+	public OnlineWorkStationAgent washer;
+	public OnlineWorkStationAgent uv;
+	public OnlineWorkStationAgent painter;
+	public OnlineWorkStationAgent baker;
+	public ConveyorAgent conv0;
 	public ConveyorAgent conv1;
+	public ConveyorAgent conv2;
+	public ConveyorAgent conv3;
+	public ConveyorAgent conv4;
+	public ConveyorAgent conv5;
+	public ConveyorAgent conv6;
+	public ConveyorAgent conv7;
+	public ConveyorAgent conv8;
+	public ConveyorAgent conv9;
+	public ConveyorAgent conv10;
+	public ConveyorAgent conv11;
+	public ConveyorAgent conv12;
+	public ConveyorAgent conv13;
+	public ConveyorAgent conv14;
 
 	/**
 	 * Constructor links this panel to its frame
@@ -93,14 +117,7 @@ public class FactoryPanel extends JPanel
 	}
 	
 	private void startAgents(){
-		gRobot = new GlassRobotAgent(transducer, cPanel.getTracePanel(),  "GlassBin");
-		conv1 = new ConveyorAgent("Cutter", transducer);
-		
-		
-		gRobot.setConveyor(conv1);
-		
-		gRobot.startThread();
-		conv1.startThread();
+
 	}
 
 	/**
@@ -108,9 +125,38 @@ public class FactoryPanel extends JPanel
 	 */
 	private void initializeBackEnd()
 	{
-		// ===========================================================================
-		// TODO initialize and start Agent threads here
-		// ===========================================================================
+		gRobot = new GlassRobotAgent(transducer, cPanel.getTracePanel(),  "GlassBin");
+		cutter = new OnlineWorkStationAgent("CUTTER", 0, 1, transducer, cPanel.getTracePanel());
+		breakout = new OnlineWorkStationAgent("BREAKOUT", 1, 1, transducer, cPanel.getTracePanel());
+		mbreakout= new OnlineWorkStationAgent("MANUAL_BREAKOUT", 2, 1, transducer, cPanel.getTracePanel());
+		crossSeamer = new PopupAgent(transducer, 1, 1 ,2);
+		grinder = new PopupAgent(transducer, 2, 1, 2);
+		drill = new PopupAgent(transducer, 3, 1, 2);
+		washer = new OnlineWorkStationAgent("WASHER", 6, 1, transducer, cPanel.getTracePanel());
+		uv = new OnlineWorkStationAgent("UV_LAMP", 7, 1, transducer, cPanel.getTracePanel());
+		painter = new OnlineWorkStationAgent("PAINTER", 8, 1, transducer, cPanel.getTracePanel());
+		baker = new OnlineWorkStationAgent("OVEN", 9, 1, transducer, cPanel.getTracePanel());
+		conv0 = new ConveyorAgent("Cutter", transducer,0);
+		conv1 = new ConveyorAgent("SHUTTLE1", transducer,1);
+		conv2 = new ConveyorAgent("BREAKOUT", transducer,2);
+		conv3 = new ConveyorAgent("MANUAL_BREAKOUT", transducer,3);
+		conv4 = new ConveyorAgent("SHUTTLE2", transducer,4);
+		conv5 = new ConveyorAgent("CROSS_SEAMER", transducer,5);
+		conv6 = new ConveyorAgent("DRILL", transducer,6);
+		conv7 = new ConveyorAgent("GRINDER", transducer,7);
+		conv8 = new ConveyorAgent("WASHER", transducer,8);
+		conv9 = new ConveyorAgent("SHUTTLE3", transducer,9);
+		conv10 = new ConveyorAgent("UV_LAMP", transducer,10);
+		conv11 = new ConveyorAgent("PAINTER", transducer,11);
+		conv12 = new ConveyorAgent("SHUTLE4", transducer,12);
+		conv13 = new ConveyorAgent("OVEN", transducer,13);
+		conv14 = new ConveyorAgent("EXIT", transducer,14);
+		
+		gRobot.setConveyor(conv1);
+		conv1.setMachines(gRobot, cutter);
+		
+		gRobot.startThread();
+		conv1.startThread();
 
 		System.out.println("Back end initialization finished.");
 	}
