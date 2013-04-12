a3de4a7b39eb53f686300581e336f4a391f2a474
Tim Huang <timhuang@usc.edu>
2013-04-12 01:52:28 -0800

    updates

--- GlassLineFactory/src/glassLine/agents/TruckAgent.java
+++ GlassLineFactory/src/glassLine/agents/TruckAgent.java
@@ -2,6 +2,7 @@ package glassLine.agents;
 
 import glassLine.Glass;
 import glassLine.interfaces.Machine;
+import gui.panels.subcontrolpanels.TracePanel;
 import transducer.TChannel;
 import transducer.TEvent;
 import transducer.Transducer;
@@ -17,24 +18,25 @@ public class TruckAgent extends Agent implements Machine {
 	private enum TruckState{loading, emptying, loaded, none};
 	private TruckState tstate;
 	
-	public TruckAgent(String name, Transducer transducer, ConveyorAgent conveyor){
+	public TruckAgent(String name, Transducer transducer, ConveyorAgent conveyor, TracePanel tp){
 		super(name);
 		this.transducer = transducer;
 		this.conveyor = conveyor;
 		this.tstate = TruckState.none;
 		transducer.register(this, TChannel.TRUCK);
+		tracePanel = tp;
 	}
 	
 	/**MESSAGES**/
 	
 	public void msgGlassIsReady(){
-		System.out.println("Truck has received msgGlassIsReady from Conveyor" + this.conveyor.getName() + "\n");
+		print("Truck has received msgGlassIsReady from Conveyor" + this.conveyor.getName() + "\n");
 		this.cstate = ConveyorState.ready;
 		stateChanged();
 	}
 	
 	public void msgHereIsGlass(Glass g){
-		System.out.println("Truck has received msgHereIsGlass from Conveyor" + this.conveyor.getName() + "\n");
+		print("Truck has received msgHereIsGlass from Conveyor" + this.conveyor.getName() + "\n");
 		this.glass = g;
 		this.transducer.fireEvent(TChannel.TRUCK, TEvent.TRUCK_DO_LOAD_GLASS, null); 
 		this.tstate = TruckState.loading;
@@ -43,14 +45,14 @@ public class TruckAgent extends Agent implements Machine {
 	}
 	
 	private void msgDoneEmptying() {
-		System.out.println("Truck has received msgDoneEmptying after animation is done emptying the glass \n");
+		print("Truck has received msgDoneEmptying after animation is done emptying the glass \n");
 		this.tstate = TruckState.none;
 		this.glass = null;
 		stateChanged();		
 	}
 
 	private void msgDoneLoading() {
-		System.out.println("Truck has received msgDoneLoading after animation is done loading \n");
+		print("Truck has received msgDoneLoading after animation is done loading \n");
 		this.tstate = TruckState.loaded;
 		stateChanged();
 	}
@@ -79,14 +81,14 @@ public class TruckAgent extends Agent implements Machine {
 
 	/**ACTIONS**/
 	private void receiveGlass(){
-		System.out.println("Truck action: receiveGlass from conveyor " +conveyor.getName() + "\n");
+		print("Truck action: receiveGlass from conveyor " +conveyor.getName() + "\n");
 		conveyor.msgReadyToTakeGlass();
 		cstate = ConveyorState.permission;
 		stateChanged();
 	}
 	
 	private void processGlass(){
-		System.out.println("Truck action: processGlass \n");
+		print("Truck action: processGlass \n");
 		this.transducer.fireEvent(TChannel.TRUCK, TEvent.TRUCK_DO_EMPTY, null); 
 		this.tstate = TruckState.emptying;
 		stateChanged();
