1f54b5935136d48ff31179cd6c91f42cbda5f407
Tim Huang <timhuang@usc.edu>
2013-04-12 02:20:36 -0800

    updates

--- GlassLineFactory/src/gui/panels/FactoryPanel.java
+++ GlassLineFactory/src/gui/panels/FactoryPanel.java
@@ -9,6 +9,7 @@ import gui.drivers.FactoryFrame;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+import transducer.TChannel;
 import transducer.Transducer;
 
 /**
@@ -141,14 +142,17 @@ public class FactoryPanel extends JPanel
 		dRobot1 = new PopupRobotAgent("DRILL",1,true,drill,transducer,cPanel.getTracePanel());
 		dRobot2 = new PopupRobotAgent("DRILL",2,false,drill,transducer,cPanel.getTracePanel());
 		drill.setRobots(dRobot1, dRobot2);
+		drill.setMachineChannel(TChannel.DRILL);
 		crossSeamer = new PopupAgent(transducer, 1, 1 ,2, cPanel.getTracePanel());
 		csRobot1 = new PopupRobotAgent("CROSS_SEAMER",1,true,crossSeamer,transducer,cPanel.getTracePanel());
 		csRobot2 = new PopupRobotAgent("CROSS_SEAMER",2,false,crossSeamer,transducer,cPanel.getTracePanel());
 		crossSeamer.setRobots(csRobot1, csRobot2);
+		crossSeamer.setMachineChannel(TChannel.CROSS_SEAMER);
 		grinder = new PopupAgent(transducer, 2, 1, 2, cPanel.getTracePanel());
 		gRobot1 = new PopupRobotAgent("GRINDER",1,true,grinder,transducer,cPanel.getTracePanel());
 		gRobot2 = new PopupRobotAgent("GRINDER",2,false,grinder,transducer,cPanel.getTracePanel());
 		grinder.setRobots(gRobot1, gRobot2);
+		grinder.setMachineChannel(TChannel.GRINDER);
 		
 		washer = new OnlineWorkStationAgent("WASHER", 1, 1, transducer, cPanel.getTracePanel());
 		painter = new OnlineWorkStationAgent("PAINTER", 2, 1, transducer, cPanel.getTracePanel());
