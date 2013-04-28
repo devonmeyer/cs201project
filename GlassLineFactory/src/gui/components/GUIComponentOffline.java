d8b615c254a068ba6b238e9017d4b357ba2f785a
mfmendiola <mmendiol@usc.edu>
2013-04-28 15:28:48 -0700

    testing event fired

--- GlassLineFactory/src/gui/components/GUIComponentOffline.java
+++ GlassLineFactory/src/gui/components/GUIComponentOffline.java
@@ -164,8 +164,7 @@ public class GUIComponentOffline extends GuiAnimationComponent implements Action
 	@Override
 	public void eventFired(TChannel channel, TEvent event, Object[] args)
 	{
-		if (event == TEvent.WORKSTATION_RELEASE_GLASS)
-			System.out.println("in release glass : " + this.part.stateBroken + args[0]);
+		System.out.println("in release glass : " + event.toString());
 		if (((Integer)args[0]).equals(index))
 		{
 			if (event == TEvent.WORKSTATION_DO_ACTION)
